ad46c74b47cf258329675d8ac0098dcc3663f24b
chanin <chaninklibpeng@gmail.com>
2015-12-05 14:32:19 +0700

    ใส่เสียงยุง เสร็จแล้ว

--- app/src/main/java/apprtc/chanin/mytraffic/MainActivity.java
+++ app/src/main/java/apprtc/chanin/mytraffic/MainActivity.java
@@ -1,13 +1,48 @@
 package apprtc.chanin.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // bind Widge
+        bindwidget();
+
+        // boutton controller
+        buttonController();
+    }// main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sound effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.mosquito);
+                buttonMediaPlayer.start();
+
+            } // event
+        });
     }
-}
+
+    private void bindwidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+}// main Class นี้ คือ คลาสหลัก
+
