dab297f4725438c4e0dcf3d8346dbf4447de538c
chanin <chaninklibpeng@gmail.com>
2015-12-06 14:02:11 +0700

    รายละเอียด activity

--- app/src/main/java/apprtc/chanin/mytraffic/MainActivity.java
+++ app/src/main/java/apprtc/chanin/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private void ListViewcontroller() {
 
         //For Icon แหล่งกำเนิดภาพ
-        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
+        final int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02,
                 R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
                 R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
                 R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.traffic_15, R.drawable.traffic_16, R.drawable.traffic_17,
                 R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
         //for Title สำหรับไตเติ้ล
-        String[] titleStrings = new String[20];
+        final String[] titleStrings = new String[20];
         titleStrings[0] = "หัวข้อที่ 1";
         titleStrings[1] = "หัวข้อที่ 2";
         titleStrings[2] = "หัวข้อที่ 3";
@@ -63,11 +64,29 @@ public class MainActivity extends AppCompatActivity {
         titleStrings[17] = "หัวข้อที่ 18";
         titleStrings[18] = "หัวข้อที่ 19";
         titleStrings[19] = "หัวข้อที่ 20";
+
         // for Detail สำหรับรายละเอียด
         String[] detailStrings = getResources().getStringArray(R.array.detail_short);
-        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, titleStrings, detailStrings, intIcon);
 
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, titleStrings, detailStrings, intIcon);
         trafficListView.setAdapter(objMyAdapter);
+
+
+
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+
+                objIntent.putExtra("Title", titleStrings[position]);
+                objIntent.putExtra("Image", intIcon[position]);
+                objIntent.putExtra("Index", position);
+                startActivity(objIntent);
+
+            }
+        });
+
+
         // ListViewcontroller
 
 
